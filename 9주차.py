5e79b79125f248635f82856e19a71c069e863cd5
gagabi47 <sahe7109@gmail.com>
2025-05-08 23:59:54 +0900

    9주차.py

--- 9주차.py
+++ 9주차.py
@@ -41,3 +41,30 @@ while True :
     print(f'장바구니에 {item}가(이) 담겼습니다.')
 
 print(f'\n>>> 장바구니 보기 : {shopping_bag}')    
+
+#연습문제 9.4
+shopping_bag = {}
+
+print('[구입]')
+while True:
+    item = input('상품명? ')
+    if item == '':  
+        break
+    count = int(input('수량은? '))
+
+    if item in shopping_bag:
+        shopping_bag[item] += count
+    else:
+        shopping_bag[item] = count
+
+    print(f'장바구니에 {item} {count}개가 담겼습니다.')
+
+print('\n>>> 장바구니 보기:', shopping_bag)
+
+print('\n[검색]')
+item = input('장바구니에서 확인하고자 하는 상품은? ')
+if item in shopping_bag:
+    print(f'{item}(은)는 {shopping_bag[item]}개 담겨 있습니다.')
+else:
+    print(f'{item}은(는) 장바구니에 없습니다.')
+
